e85f2d81d1231cb9546178942d3a72ce94b042a1
Gurunathan Thangavelu <gurunathan6378@hotmail.com>
2017-07-11 19:39:30 -0700

    HeapSortAlogrithm

--- WindowsFormsApplication3/Sorting.cs
+++ WindowsFormsApplication3/Sorting.cs
@@ -290,33 +290,96 @@ namespace WindowsFormsApplication3
             Merge_Sort_Top_down_implementation();
         }
 
-
-        
-        public void HeapSortAlgorithm(int[] input, int lower, int upper)
+        private int IParent(int count)
         {
-            if (input != null && input.Length > 0 && lower < upper)
-            {
-                int i = this.HeapSortPartition(input, lower, upper);
-                HeapSortAlgorithm(input, lower, i-1);
-                HeapSortAlgorithm(input, i, upper);
-            }
+            return (count - 1) / 2;
         }
-        
-        public int HeapSortPartition(int[] input, int lower, int upper)
+
+        private int ILeft(int count)
         {
-            var i = lower;
-            for (var s = lower; s < upper; s++)
+            return ((2* count) + 1);
+        }
+
+        private int IRight(int count)
+        {
+            return ((2 * count) + 2);
+        }
+
+        private void ShiftDown(int[]a, int start, int end)
+        {
+            int root = start;
+            int child = 0;
+            int interChange = 0;
+
+            while(this.ILeft(root) <= end) //do    (While the root has at least one child)
+
             {
-                if (input[s] <= input[upper])
+                child = this.ILeft(root); // (Left child of root)
+                interChange = root;               // (Keeps track of child to swap with)
+
+                if (a[interChange] < a[child])
                 {
-                    this.Swap(input, s, i);
-                    i++;
+                    interChange = child;
+                }
+
+                //(If there is a right child and that child is greater)
+                if ((child + 1) <= end && a[interChange] < a[child + 1])
+		        {
+                    interChange = child + 1;
+                }
+
+                if (interChange == root)
+                {
+                    //(The root holds the largest element.Since we assume the heaps rooted at the
+                    //children are valid, this means that we are done.)
+                    return;
+                }
+                else
+                {
+                    this.Swap(a, root, interChange);
+                    root = interChange; //(repeat to continue sifting down the child now)
                 }
             }
-            this.Swap(input, upper, i);            
-            return i;
         }
 
+
+        public void Heapify(int[] a, int count)
+        {
+            //(start is assigned the index in 'a' of the last parent node)
+            //(the last element in a 0-based array is at index count-1; find the parent of that element)
+            int start = this.IParent(count - 1);
+
+            while (start >= 0)
+            {
+                //(sift down the node at index 'start' to the proper place such that all nodes below the start index are in heap order)
+                this.ShiftDown(a, start, count - 1);
+                //(go to the next parent node)
+                start--;
+                //(after sifting down the root all nodes/elements are in heap order)
+            }
+        }
+
+        public void HeapSortAlogrithm(int[] a, int count)
+        {
+            //input: an unordered array a of length count
+
+            //(Build the heap in array a so that largest value is at the root)
+            this.Heapify(a, count);
+
+            //(The following loop maintains the invariants that a[0:end] is a heap and every element
+            //beyond end is greater than everything before it (so a[end:count] is in sorted order))
+            int end = count - 1;
+            while(end > 0)        
+            {
+                //(a[0] is the root and largest value. The swap moves it in front of the sorted elements.)
+                this.Swap(a , end, 0);
+                //(the heap size is reduced by one)
+                end = end - 1;
+                //(the swap ruined the heap property, so restore it)
+                this.ShiftDown(a, 0, end);
+        
+            }
+        }
 
         private void HeapSort_Click(object sender, EventArgs e)
         {
@@ -325,7 +388,7 @@ namespace WindowsFormsApplication3
             StringBuilder result = new StringBuilder();
             result.AppendLine($"Before Sorting {DisplayResult(input)}");
 
-            HeapSortAlgorithm(input, 0, input.Length - 1);
+            this.HeapSortAlogrithm(input, input.Length);
             result.AppendLine($"After Sorting {DisplayResult(input)}");
             MessageBox.Show(result.ToString());
         }
