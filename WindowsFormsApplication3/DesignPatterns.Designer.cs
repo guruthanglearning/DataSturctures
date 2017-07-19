7eac9dada85e8de514cdb8b792fc5ff9b341fab7
gthanga <gthanga@NB0077-R90GMBV6>
2017-07-19 07:16:51 -0700

    Added forms for Design patterns --Added singleton

---
+++ WindowsFormsApplication3/DesignPatterns.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class DesignPatterns
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.singleton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // singleton
+            // 
+            this.singleton.Location = new System.Drawing.Point(19, 30);
+            this.singleton.Name = "singleton";
+            this.singleton.Size = new System.Drawing.Size(182, 35);
+            this.singleton.TabIndex = 0;
+            this.singleton.Text = "SingleTon";
+            this.singleton.UseVisualStyleBackColor = true;
+            this.singleton.Click += new System.EventHandler(this.singleton_Click);
+            // 
+            // DesignPatterns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 551);
+            this.Controls.Add(this.singleton);
+            this.Name = "DesignPatterns";
+            this.Text = "DesignPatterns";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button singleton;
+    }
+}
---
+++ WindowsFormsApplication3/DesignPatterns.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class DesignPatterns : Form
+    {
+        public DesignPatterns()
+        {
+            InitializeComponent();
+        }
+
+        private void singleton_Click(object sender, EventArgs e)
+        {
+            Singleton.
+        }
+    }
+
+
+    public sealed class Singleton
+    {
+        private static volatile Singleton instance;
+        private static object syncRoot = new Object();
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public string TestProperty
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
