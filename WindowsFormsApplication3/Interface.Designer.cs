d0e2b2cd0336394df126cd0b96d0f1e28c366c88
gurunathan6378@hotmail.com <babi7836>
2017-08-24 16:08:20 -0700

    Added Interface forms for Interface related learning

---
+++ WindowsFormsApplication3/Interface.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Making_Interface_Method_in_Derived_Class_As_Private = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Making_Interface_Method_in_Derived_Class_As_Private
+            // 
+            this.Making_Interface_Method_in_Derived_Class_As_Private.Location = new System.Drawing.Point(12, 12);
+            this.Making_Interface_Method_in_Derived_Class_As_Private.Name = "Making_Interface_Method_in_Derived_Class_As_Private";
+            this.Making_Interface_Method_in_Derived_Class_As_Private.Size = new System.Drawing.Size(419, 29);
+            this.Making_Interface_Method_in_Derived_Class_As_Private.TabIndex = 0;
+            this.Making_Interface_Method_in_Derived_Class_As_Private.Text = "Making Interface Method in Derived Class As Private";
+            this.Making_Interface_Method_in_Derived_Class_As_Private.UseVisualStyleBackColor = true;
+            this.Making_Interface_Method_in_Derived_Class_As_Private.Click += new System.EventHandler(this.Making_Interface_Method_in_Derived_Class_As_Private_Click);
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 546);
+            this.Controls.Add(this.Making_Interface_Method_in_Derived_Class_As_Private);
+            this.Name = "Interface";
+            this.Text = "Interface";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Making_Interface_Method_in_Derived_Class_As_Private;
+    }
+}
---
+++ WindowsFormsApplication3/Interface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Interface : Form
+    {
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Making_Interface_Method_in_Derived_Class_As_Private_Click(object sender, EventArgs e)
+        {
+            // When we try to instaniate from Interface then Notifier becomes available when we try to 
+            // instaniate from Notify class then Notify method does not become available.
+            INotify n = new Notify();
+            n.Notify();
+        }
+    }
+
+    public interface INotify
+    {
+        void Notify();
+    }
+
+    public class Notify : INotify
+    {
+        void INotify.Notify()
+        {
+            MessageBox.Show("Made INotify.Notify as private method");
+        }
+    }
+
+}
