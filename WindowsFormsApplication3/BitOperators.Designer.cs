cbb7868ad646b9f682c5e0642b8e8c3a48a39ee3
gurunathan6378@hotmail.com <babi7836>
2017-11-16 10:21:33 -0800

    Added BitOperators form

---
+++ WindowsFormsApplication3/BitOperators.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class BitOperators
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCompute_XOR_for_the_given_number = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCompute_XOR_for_the_given_number
+            // 
+            this.btnCompute_XOR_for_the_given_number.Location = new System.Drawing.Point(12, 12);
+            this.btnCompute_XOR_for_the_given_number.Name = "btnCompute_XOR_for_the_given_number";
+            this.btnCompute_XOR_for_the_given_number.Size = new System.Drawing.Size(393, 42);
+            this.btnCompute_XOR_for_the_given_number.TabIndex = 0;
+            this.btnCompute_XOR_for_the_given_number.Text = "Compute XOR for the given number";
+            this.btnCompute_XOR_for_the_given_number.UseVisualStyleBackColor = true;
+            this.btnCompute_XOR_for_the_given_number.Click += new System.EventHandler(this.btnCompute_XOR_for_the_given_number_Click);
+            // 
+            // BitOperators
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 500);
+            this.Controls.Add(this.btnCompute_XOR_for_the_given_number);
+            this.Name = "BitOperators";
+            this.Text = "BitOperators";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCompute_XOR_for_the_given_number;
+    }
+}
---
+++ WindowsFormsApplication3/BitOperators.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class BitOperators : Form
+    {
+
+        /* Bitwise operations
+         * 
+            	    1	0	0               1   0   0           1   0   0
+	                1	1	0               1   1   0           1   1   0
+                    ----------              ---------           ----------
+              Add   1	0	0           OR  1   1   0       XOR 0   1   0                                        
+        */
+        public BitOperators()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompute_XOR_for_the_given_number_Click(object sender, EventArgs e)
+        {
+            int input = 6;
+            int result = 0;
+            if (input % 4 == 0)
+            {
+                result = input;
+            }
+            else if (input%4 == 1)
+            {
+                result = 1;
+            }
+            else if (input%4 == 2)
+            {
+                result = ++input;
+            }
+
+            MessageBox.Show(result.ToString());
+            
+
+        }
+    }
+}
