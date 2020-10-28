9af19915aeee0b6f03642c6f0825cb40cce1c137
Gurunathan Thangavelu <gurunathan6378@hotmail.com>
2020-10-27 18:01:32 -0700

    Added DS on 10_27_2020

--- WindowsFormsApplication3/LinkedListForm.cs
+++ WindowsFormsApplication3/LinkedListForm.cs
@@ -5031,6 +5031,135 @@ namespace WindowsFormsApplication3
 
         }
 
+        private void btn_Linked_List_Cycle_II_Click(object sender, EventArgs e)
+        {
+            /*
+        
+                Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+                There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+                Notice that you should not modify the linked list.
+
+                Follow up:
+
+                Can you solve it using O(1) (i.e. constant) memory?
+
+ 
+
+                Example 1:
+                Input: head = [3,2,0,-4], pos = 1
+                Output: tail connects to node index 1
+                Explanation: There is a cycle in the linked list, where tail connects to the second node.
+                
+                3-->2-->0-->-4
+                    |--------|
+
+
+                Example 2:
+                Input: head = [1,2], pos = 0
+                Output: tail connects to node index 0
+                Explanation: There is a cycle in the linked list, where tail connects to the first node.
+                Example 3:
+
+                Input: head = [1], pos = -1
+                Output: no cycle
+                Explanation: There is no cycle in the linked list.
+ 
+
+                Constraints:
+
+                The number of the nodes in the list is in the range [0, 104].
+                -105 <= Node.val <= 105
+                pos is -1 or a valid index in the linked-list.
+
+
+                Time Complexity     :   O(N)
+                Space Complexity    :   O(1)
+
+            */
+
+
+            StringBuilder result = new StringBuilder();
+            List<Common> inputs = new List<Common>();            
+            inputs.Add(new Common() { Node1 = this.InsertListNode(new int[] { 3, 2, 0, 4 }), Find = 2 });
+            inputs.Add(new Common() { Node1 = this.InsertListNode(new int[] { 1, 2 }), Find = 1 });
+            inputs.Add(new Common() { Node1 = this.InsertListNode(new int[] { 1 }), Find = 0 });
+
+
+            ListNode resNode = null;
+            foreach (var input in inputs)
+            {
+                
+                result.AppendLine($"Linked List Cycle II for the given list {this.GetListNodeData(input.Node1)}  is ");
+                resNode = this.DetectCycle(MakeCyclicLinkedList(input.Node1, input.Find));
+                result.AppendLine($"{(resNode == null ? "not exists" : resNode.val.ToString())}");
+            }
+
+            MessageBox.Show(result.ToString());
+
+        }
+
+
+        public ListNode MakeCyclicLinkedList(ListNode node, int index)
+        {
+            if (node == null || index == 0)
+                return node;
+
+            ListNode runner = node;
+            ListNode indexNode = node;
+
+
+            while (runner.next!= null)
+            {
+                if (index-1 > 0)
+                {
+                    indexNode = indexNode.next;
+                    index--;
+                }
+
+                runner = runner.next;
+            }
+
+            runner.next = indexNode;
+
+            return node;
+
+        }
+
+
+        public ListNode DetectCycle(ListNode head)
+        {
+
+            if (head == null)
+                return head;
+
+            ListNode slow = head;
+            ListNode fast = slow;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    slow = head;
+
+                    while (slow != fast)
+                    {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+
+                    return slow;
+                }
+
+            }
+
+
+            return null;
+        }
     }
 
 
