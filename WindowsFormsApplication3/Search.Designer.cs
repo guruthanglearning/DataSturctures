cd29d5c17662200ecc4a094dac3b3dc75714eaa5
Gurunathan Thangavelu <gurunathan6378@hotmail.com>
2020-11-20 15:16:54 -0800

    Added search in duplicated rotating array as of 11_20_2020

--- WindowsFormsApplication3/Search.Designer.cs
+++ WindowsFormsApplication3/Search.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.btn_BinarySearch = new System.Windows.Forms.Button();
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists = new System.Windows.Forms.Button();
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_BinarySearch
             // 
-            this.btn_BinarySearch.Location = new System.Drawing.Point(12, 11);
-            this.btn_BinarySearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_BinarySearch.Location = new System.Drawing.Point(9, 9);
             this.btn_BinarySearch.Name = "btn_BinarySearch";
-            this.btn_BinarySearch.Size = new System.Drawing.Size(669, 32);
+            this.btn_BinarySearch.Size = new System.Drawing.Size(117, 26);
             this.btn_BinarySearch.TabIndex = 0;
             this.btn_BinarySearch.Text = "Binary Search";
             this.btn_BinarySearch.UseVisualStyleBackColor = true;
@@ -45,23 +45,35 @@
             // 
             // btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists
             // 
-            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Location = new System.Drawing.Point(12, 50);
-            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Location = new System.Drawing.Point(9, 41);
+            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Name = "btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists";
-            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Size = new System.Drawing.Size(669, 34);
+            this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Size = new System.Drawing.Size(117, 54);
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.TabIndex = 1;
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Text = "Given an rotate array in the sorted order search  given value exists";
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.UseVisualStyleBackColor = true;
             this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists.Click += new System.EventHandler(this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists_Click);
             // 
+            // btn_Search_in_Rotated_Sorted_Array_Duplicate
+            // 
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Location = new System.Drawing.Point(9, 99);
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Name = "btn_Search_in_Rotated_Sorted_Array_Duplicate";
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Size = new System.Drawing.Size(117, 52);
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.TabIndex = 2;
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Text = "Given an rotate array in the sorted order(duplicate)\r\n search  given value exists" +
+    "";
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.UseVisualStyleBackColor = true;
+            this.btn_Search_in_Rotated_Sorted_Array_Duplicate.Click += new System.EventHandler(this.btn_Search_in_Rotated_Sorted_Array_Duplicate_Click);
+            // 
             // Search
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 322);
+            this.ClientSize = new System.Drawing.Size(560, 262);
+            this.Controls.Add(this.btn_Search_in_Rotated_Sorted_Array_Duplicate);
             this.Controls.Add(this.btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists);
             this.Controls.Add(this.btn_BinarySearch);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Search";
             this.Text = "Search";
             this.ResumeLayout(false);
@@ -72,5 +84,6 @@
 
         private System.Windows.Forms.Button btn_BinarySearch;
         private System.Windows.Forms.Button btn_Given_an_split_array_in_the_sorted_order_search_given_value_exists;
+        private System.Windows.Forms.Button btn_Search_in_Rotated_Sorted_Array_Duplicate;
     }
 }
--- WindowsFormsApplication3/Search.cs
+++ WindowsFormsApplication3/Search.cs
@@ -93,11 +93,12 @@ namespace WindowsFormsApplication3
             StringBuilder output = new StringBuilder(); 
             List<ArrayContainterForSearch> inputs = new List<ArrayContainterForSearch>();
             //inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 1, 2, 3, 4, 5, 6 }, Search = 6 });
-            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 3, 4, 5, 6, 1, 2 }, Search = 4 });
+            //inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 3, 4, 5, 6, 1, 2 }, Search = 4 });
             //inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 3, 4, 5, 6, 7, 1, 2 }, Search = 3 });
             //inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 5, 6, 7, 8, 9, 10, 1, 2, 3, 4 }, Search = 11 });
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 1, 3, 1, 1, 1 }, Search = 3 });
 
-            
+
             int left = 0; int right = 0;
             int mid = 0;
             int pivot = -1;
@@ -168,5 +169,88 @@ namespace WindowsFormsApplication3
             public int Search;
         }
 
+        private void btn_Search_in_Rotated_Sorted_Array_Duplicate_Click(object sender, EventArgs e)
+        {
+
+
+            /*
+                Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+                (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+                You are given a target value to search. If found in the array return true, otherwise return false.
+
+                Example 1:
+
+                Input: nums = [2,5,6,0,0,1,2], target = 0
+                Output: true
+                Example 2:
+
+                Input: nums = [2,5,6,0,0,1,2], target = 3
+                Output: false
+                Follow up:
+
+                This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+                Would this affect the run-time complexity? How and why?
+             
+                Time Complexity         : O(log N)
+                Space Complexity        : O(1)
+             
+             */
+
+
+            StringBuilder result = new StringBuilder();
+            List<ArrayContainterForSearch> inputs = new List<ArrayContainterForSearch>();
+
+
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 2, 5, 6, 0, 0, 1, 2 }, Search = 0 });
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 2, 5, 6, 0, 0, 1, 2 }, Search = 3 });
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 1 }, Search = 1 });
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 3,1 }, Search = 1 });
+            inputs.Add(new ArrayContainterForSearch() { Input = new int[] { 1, 3, 1, 1, 1 }, Search = 3 });
+
+            
+            foreach (var input in inputs)
+            {
+
+                result.AppendLine($"Given number {input.Search} for rotating array with duplicate  {string.Join(",", input.Input)} does {(this.SearchNumberInRotatingArrayWithDuplicate(input.Input, input.Search) ? "" : " not ")} exists ");
+            }
+
+            MessageBox.Show(result.ToString());
+        }
+
+        public bool SearchNumberInRotatingArrayWithDuplicate(int[] nums, int target)
+        {
+
+            if (nums == null || nums.Length == 0)
+                return false;
+            if (nums.Length == 0 || nums == null) return false;
+            int l = 0, r = nums.Length - 1, mid = 0;
+            while (l <= r)
+            {
+                mid = (l + r) / 2;
+                if (nums[mid] == target) return true;
+                else if (nums[l] < nums[mid])
+                {
+                    if (nums[l] <= target && nums[mid] > target)                    
+                        r = mid - 1;
+                    else 
+                        l = mid + 1;
+                }
+                else if (nums[l] > nums[mid])
+                {
+                    if (nums[r] >= target && nums[mid] < target) 
+                        l = mid + 1;
+                    else 
+                        r = mid - 1;
+                }
+                else 
+                    l++;
+            }
+            return false;
+
+
+
+        }
     }
 }
