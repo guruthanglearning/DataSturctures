5c12a9e5bee2f0706733be1c565c3356efde6d45
Gurunathan Thangavelu <gurunathan6378@hotmail.com>
2021-01-15 13:56:24 -0800

    Added Get Maximum in Generated Array as of 01_15_2021

--- WindowsFormsApplication3/Integers.cs
+++ WindowsFormsApplication3/Integers.cs
@@ -2075,6 +2075,117 @@ namespace WindowsFormsApplication3
                 }
             }
         }
+
+        private void btn_Get_Maximum_in_Generated_Array_Click(object sender, EventArgs e)
+        {
+            /*
+                You are given an integer n. An array nums of length n + 1 is generated in the following way:
+
+                nums[0] = 0
+                nums[1] = 1
+                nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+                nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+                Return the maximum integer in the array nums​​​.
+
+                Example 1:
+
+                Input: n = 7
+                Output: 3
+                Explanation: According to the given rules:
+                  nums[0] = 0
+                  nums[1] = 1
+                  nums[(1 * 2) = 2] = nums[1] = 1
+                  nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+                  nums[(2 * 2) = 4] = nums[2] = 1
+                  nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+                  nums[(3 * 2) = 6] = nums[3] = 2
+                  nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+                Hence, nums = [0,1,1,2,1,3,2,3], and the maximum is 3.
+                Example 2:
+
+                Input: n = 2
+                Output: 1
+                Explanation: According to the given rules, the maximum between nums[0], nums[1], and nums[2] is 1.
+                Example 3:
+
+                Input: n = 3
+                Output: 2
+                Explanation: According to the given rules, the maximum between nums[0], nums[1], nums[2], and nums[3] is 2.
+ 
+
+                Constraints:
+
+                0 <= n <= 100
+                   Hide Hint #1  
+                Try generating the array.
+                   Hide Hint #2  
+                Make sure not to fall in the base case of 0. 
+
+                Time Complexity     :   O(N)
+                Space Complexity    :   O(N)
+
+             */
+
+
+            List<int> inputs = new List<int>();
+            inputs.Add(7);
+            inputs.Add(2);
+            inputs.Add(3);
+            inputs.Add(1);
+            StringBuilder result = new StringBuilder();
+            foreach (var input in inputs)
+            {
+                result.AppendLine($"Get Maximum in Generated Array is { this.GetMaximumGenerated(input)} for the given integer {input}");
+            }
+
+            MessageBox.Show(result.ToString());
+        }
+
+        public int GetMaximumGenerated(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            int temp = 0;
+            int[] result = new int[n + 1];
+            result[0] = 0;
+            result[1] = 1;
+
+            int max = 1;
+            
+
+            /*
+
+                  nums[(1 * 2) = 2] = nums[1] = 1
+                  nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+                  nums[(2 * 2) = 4] = nums[2] = 1
+                  nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+                  nums[(3 * 2) = 6] = nums[3] = 2
+                  nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+
+            */
+
+            for (int i = 1; i <= n; i++)
+            {
+                temp = i * 2;
+                if (temp + 1 > n)
+                    break;
+                result[temp] = result[i];
+
+                result[temp + 1] = result[i] + result[i + 1];
+                max = Math.Max(max, Math.Max(result[temp], result[temp + 1]));
+
+                
+
+            }
+
+            return max;
+
+
+
+        }
     }
 }
 
