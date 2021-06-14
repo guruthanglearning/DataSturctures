ca83316053e8ffef4f978a5ffd9a3de806954d7f
gurunathan6378@hotmail.com <gurunathan6378@hotmail.com>
2021-06-14 16:27:49 -0700

    Added Maximum Units on a Truck problem

--- WindowsFormsApplication3/Matrix.cs
+++ WindowsFormsApplication3/Matrix.cs
@@ -4030,5 +4030,121 @@ namespace WindowsFormsApplication3
             return islandSize;
 
         }
+
+        private void btn_Maximum_Units_on_a_Truck_Click(object sender, EventArgs e)
+        {
+            /*
+            
+                    You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+                    numberOfBoxesi is the number of boxes of type i.
+                    numberOfUnitsPerBoxi is the number of units in each box of the type i.
+                    You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+                    Return the maximum total number of units that can be put on the truck.
+
+ 
+
+                    Example 1:
+
+                    Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+                    Output: 8
+                    Explanation: There are:
+                    - 1 box of the first type that contains 3 units.
+                    - 2 boxes of the second type that contain 2 units each.
+                    - 3 boxes of the third type that contain 1 unit each.
+                    You can take all the boxes of the first and second types, and one box of the third type.
+                    The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+                    Example 2:
+
+                    Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+                    Output: 91
+ 
+
+                    Constraints:
+
+                    1 <= boxTypes.length <= 1000
+                    1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+                    1 <= truckSize <= 106
+                       Hide Hint #1  
+                    If we have space for at least one box, it's always optimal to put the box with the most units.
+                       Hide Hint #2  
+                    Sort the box types with the number of units per box non-increasingly.
+                       Hide Hint #3  
+                    Iterate on the box types and take from each type as many as you can.
+             
+
+                    TC  : O(NLogN)
+                    SC  : O(1)
+             
+             */
+
+
+
+            StringBuilder result = new StringBuilder();
+            List<Common> inputs = new List<Common>();
+
+
+            inputs.Add(new Common()
+            {
+                Input = new int[][] {
+                                          new int[]{1,3},
+                                          new int[]{2,2},
+                                          new int[]{3,1}
+
+                                    },N= 4
+            });
+
+            inputs.Add(new Common()
+            {
+                Input = new int[][] {
+                                          new int[]{ 5, 10 },
+                                          new int[]{ 2,5 },
+                                          new int[]{ 4,7 },
+                                          new int[]{ 3,9 }
+                                    },
+                N = 10
+            });
+
+
+
+            foreach (var input in inputs)
+            {
+                result.AppendLine($"Maximum Units on a Truck : {Environment.NewLine} {this.PrintJaggedArray(input.Input)} {Environment.NewLine} {this.MaximumUnits(input.Input, input.N)}");
+
+            }
+
+            MessageBox.Show(result.ToString());
+        }
+
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+
+            if (boxTypes == null || boxTypes.Length == 1)
+                return 0;
+
+            Array.Sort(boxTypes, (x, y) => y[1] - x[1]);
+
+            int maxUnits= 0;
+            int noOfBoxes = 0;
+
+            foreach(var i in boxTypes)
+            {
+
+                if (noOfBoxes + i[0] <= truckSize)
+                {
+                    noOfBoxes += i[0];
+                    maxUnits += (i[0] * i[1]);
+                }
+                else
+                {
+                    maxUnits += (truckSize - noOfBoxes) * i[1];
+                    return maxUnits;
+                }                
+            }
+
+            return maxUnits;
+        }
+
     }
 }
