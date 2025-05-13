e252c3c6a381d2095abc3b5cd8fdb6a0cc034a47
Gurunathan Thangavelu <gurunathan6378@hotmail.com>
2025-05-13 14:33:55 -0700

    Added  solution for "Divide Two Integers" problem

--- WindowsFormsApplication3/Integers.cs
+++ WindowsFormsApplication3/Integers.cs
@@ -2683,6 +2683,97 @@ namespace WindowsFormsApplication3
             return result.ToString();
         }
 
+        private void btn_Divide_Two_Integers_Click(object sender, EventArgs e)
+        {
+
+            /*
+                Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+                The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+                Return the quotient after dividing dividend by divisor.
+
+                Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+ 
+
+                Example 1:
+
+                Input: dividend = 10, divisor = 3
+                Output: 3
+                Explanation: 10/3 = 3.33333.. which is truncated to 3.
+                Example 2:
+
+                Input: dividend = 7, divisor = -3
+                Output: -2
+                Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+ 
+
+                Constraints:
+
+                -231 <= dividend, divisor <= 231 - 1
+                divisor != 0
+
+                Time Complexity : O(log(N))
+                Space Complexity : O(1)
+            */
+
+            Tuple<int, int>[] inputs = new Tuple<int, int>[] {
+                //new Tuple<int, int>(10, 3),
+                //new Tuple<int, int>(7, -3),
+                //new Tuple<int, int>(1, 1),
+                //new Tuple<int, int>(-1, -1),
+                //new Tuple<int, int>(-2147483648, -1),
+                //new Tuple<int, int>(-2147483648, 1),
+                //new Tuple<int, int>(-2147483648, 2),
+                new Tuple<int, int>(-1010369383,-2147483648)
+            };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var input in inputs)
+            {
+                result.AppendLine($"Divide Two Integers for the given dividend {input.Item1} and divisor {input.Item2} is {this.Divide(input.Item1, input.Item2)} ");
+            }
+
+            MessageBox.Show(result.ToString());
+
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            long result = 0;
+            long temp = 0;
+            long multiple = 0;
+
+            if (dividend == int.MinValue && divisor == -1)
+                return int.MaxValue;
+            else if (dividend == int.MinValue && divisor == 1)
+                return int.MinValue;
+
+            bool isNeg = (dividend < 0) ^ (divisor < 0);
+
+            long a = Math.Abs((long)dividend);
+            long b = Math.Abs((long)divisor);
+
+            while (a >= b) 
+            {
+                temp = b;
+                multiple = 1;
+
+                while (a >= temp << 1) 
+                {
+                    temp <<= 1;
+                    multiple <<= 1;              
+                }
+
+                a -= temp; 
+                result += multiple;
+
+            }
+
+            return isNeg ? -1 * (int)result : (int)result;
+
+        }
     }
 }
 
